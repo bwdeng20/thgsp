7adaa4812827dbbe2db736ddd7a58a1f1a63d3fd
Bowen Deng <bowen.deng20@gmail.com>
2021-04-12 21:39:32 +0800

    feat(Graph Sampling):
    Introduce graph sampling method proposed by Aamir Anis, et al. into
    thgsp.

---
+++ tests/test_sampling/test_ess.py
@@ -0,0 +1,90 @@
+import pytest
+import numpy as np
+import torch as th
+from ..utils4t import float_dtypes, lap_types, devices
+from thgsp.sampling.ess import ess_sampling, power_iteration, power_iteration4min, ess
+from thgsp.graphs import rand_udg, laplace
+import scipy.sparse as sparse
+from scipy.sparse.linalg import eigsh
+
+np.set_printoptions(precision=5)
+th.set_printoptions(precision=5)
+
+
+@pytest.mark.parametrize('dtype', float_dtypes[1:])
+@pytest.mark.parametrize('device', devices)
+@pytest.mark.parametrize('lap', lap_types)
+class TestPowerIteration:
+    def test_power_iteration_max(self, dtype, device, lap):
+        N = 20
+        k = 2
+        g = rand_udg(N, dtype=dtype, device=device).set_value_(None)
+        L = laplace(g, lap_type=lap)
+        Sc = th.randint(N, (N // 2,))
+        val, vec = power_iteration(L, Sc, k, num_iter=100)
+        L = L.to_scipy()
+        LtL = L.T ** k * L ** k
+        reduced = LtL[np.ix_(Sc, Sc)]
+        sigma, psi = eigsh(reduced, k=1, which="LM")
+        sigma = sigma[0]
+        print(f"\nth vs sci: {val:.4f}:{sigma:.4f}")
+        print(f"th vs sci: \n", vec.view(-1), "\n", psi.ravel())
+        assert (sigma - val) ** 2 < 1e-3
+
+    def test_power_iteration_shift(self, dtype, device, lap):
+        N = 20
+        k = 3
+        shift = 1.5
+        g = rand_udg(N, dtype=dtype, device=device).set_value_(None)
+        L = laplace(g, lap_type=lap)
+        Sc = th.randint(N, (N // 2,))
+        val, vec = power_iteration(L, Sc, k, shift=shift, num_iter=200)
+        L = L.to_scipy()
+        LtL = L.T ** k * L ** k
+        reduced = LtL[np.ix_(Sc, Sc)] - shift * sparse.eye(len(Sc), dtype=LtL.dtype)
+        sigma, psi = eigsh(reduced, k=1, which="LM")
+        sigma = sigma[0]
+        print(f"\nth vs sci: {val:.4f}:{sigma:.4f}")
+        print(f"th vs sci: \n", vec.view(-1), "\n", psi.ravel())
+        assert (sigma - val) ** 2 < 1e-2
+
+    def test_power_iteration_min(self, dtype, device, lap):
+        if lap is 'comb':
+            pytest.skip('combinatorial Laplacian operator is not numerically stable according to many experiments')
+        N = 200
+        k = 2
+        g = rand_udg(N, dtype=dtype, device=device).set_value_(None)
+        L = laplace(g, lap_type=lap)
+        Sc = th.randint(N, (N // 2,))
+        val, vec = power_iteration4min(L, Sc, k, num_iter=500)
+        L = L.to_scipy()
+        LtL = L.T ** k * L ** k
+        reduced = LtL[np.ix_(Sc, Sc)]
+        sigma, psi = eigsh(reduced, k=1, which="SM")
+        sigma = sigma[0]
+        print(f"\nth vs sci: {val:.4f}:{sigma:.4f}")
+        print(f"th vs sci: \n", vec.view(-1), "\n", psi.ravel())
+        assert (sigma - val) ** 2 < 1e-3
+
+
+@pytest.mark.parametrize('dtype', float_dtypes[1:])  # omit float32 since it may lead to ArpackNoConvergence error
+@pytest.mark.parametrize('device', devices)
+@pytest.mark.parametrize('lap', lap_types)
+class TestEss:
+    def test_ess_sampling(self, dtype, device, lap):
+        N = 30
+        M = 10
+        k = 2
+        g = rand_udg(N, dtype=dtype, device=device)
+        L = laplace(g, lap_type=lap)
+        S = ess_sampling(L, M, k)
+        print(S)
+
+    def test_ess(self, dtype, device, lap):
+        N = 30
+        M = 10
+        k = 2
+        g = rand_udg(N, dtype=dtype, device=device)
+        L = laplace(g, lap_type=lap)
+        S = ess(L, M, k)
+        print(S)
---
+++ thgsp/sampling/ess.py
@@ -0,0 +1,94 @@
+import numpy as np
+import torch as th
+from scipy.sparse.linalg import eigsh, ArpackNoConvergence
+from thgsp.convert import to_scipy, SparseTensor
+from thgsp.utils import consecutive_spmv
+
+
+def ess_sampling(operator, M, k=2):
+    L = to_scipy(operator)
+    N = L.shape[-1]
+    LtL = L.T ** k * L ** k
+    V = np.arange(N)
+    S = list()
+    while len(S) < M:
+        Sc = np.setdiff1d(V, S)
+        reduced = LtL[np.ix_(Sc, Sc)]
+        try:
+            sigma, psi = eigsh(reduced, k=1, which="SM")
+        except ArpackNoConvergence as err:
+            raise err
+        psi = psi.ravel()
+        v = Sc[np.argmax(psi ** 2)]
+        S.append(v)
+    return S
+
+
+def ess(operator, M, k=2, max_iter=int(5e2)):
+    r"""
+    An efficient sampling set selection method for bandlimited graph signals.
+
+    Parameters
+    ----------
+    operator: SparseTensor
+        The chosen variation operators, e.g., graph normalized Laplacian.
+    M:  int
+        The number of desired sampled nodes.
+    k:  int
+        The proxy order. Refer to the literature for details.
+    max_iter:   int
+        The maximum number of iterations acceptable in Power Iteration.
+
+    Returns
+    -------
+    S:  list
+        A list containing sampled nodes with the sampling order
+
+    References
+    ----------
+    .. [1]  Aamir Anis, Akshay Gadde, and Antonio Ortega, “Efficient sampling set selection for bandlimited graph
+            signals using graph spectral proxies,” IEEE Trans on Signal Processing, 2016.
+
+    """
+    N = operator.size(-1)
+    V = np.arange(N)
+    S = []
+    while len(S) < M:
+        Sc = np.setdiff1d(V, S)
+        sigma, psi = power_iteration4min(operator, Sc, k, max_iter)
+        v = Sc[th.argmax(psi ** 2)]
+        S.append(v)
+    return S
+
+
+def power_iteration(L: SparseTensor, Sc: iter, k=2, shift=0, num_iter=50, tol=1e-6):
+    Sc = th.as_tensor(Sc)
+    Lt = L.t()
+    I = SparseTensor.eye(L.size(0), dtype=L.dtype(), device=L.device())
+    Isv = I[Sc, :]
+    Ivs = I[:, Sc]
+    x0 = th.rand(len(Sc), 1, device=L.device(), dtype=L.dtype())
+    for _ in range(num_iter):
+        x1 = Ivs @ x0
+        x1 = consecutive_spmv(L, x1, k)
+        x1 = consecutive_spmv(Lt, x1, k)
+        x1 = Isv @ x1
+        if shift != 0:
+            x1 = x1 - shift * x0
+        x1 = x1 / x1.norm()
+        if (x1 - x0).norm() < tol:
+            break
+        x0 = x1
+    t = Ivs @ x0
+    t = consecutive_spmv(L, t, k)
+    lam = (t ** 2).sum()
+    if shift != 0:
+        lam = lam - shift * (x0 ** 2).sum()
+    return lam.item(), x0
+
+
+def power_iteration4min(L: SparseTensor, Sc: iter, k=2, num_iter=50):
+    lam_max, _ = power_iteration(L, Sc, k, num_iter=num_iter)
+    lam_min_minus_max, v = power_iteration(L, Sc, k, shift=lam_max, num_iter=num_iter)
+    lam_min = lam_min_minus_max + lam_max
+    return lam_min, v
